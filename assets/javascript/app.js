a8715ff0d918e72c858ff147e8bdbdbf3e7c7a61
vsbakula <goldenrhesus2022@gmail.com>
2019-04-12 21:30:42 -0700

    var's ?,Anwser

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// array of questions
+var musicQuestions = ["what was a 1970 Music Awards for artistic & Technical merits?", "Alanis Morissette first Album in 1976?", "who won the 39th Grammy Awards with 4 amazing wins?", "the Top 1960's Beatles Album of all times? ", "What was the Billboards # 8 Single song hit of 1980's?", "Who is the best Female Singer of all time?", "Top Female Guitarist of all time is?"];
+
+// array of correct answers
+var correctChoices = ["Academy Awards", "Dance Pop", "Baby Face", " Sgt. Pepper's Lonely Hearts Band", "Crazy Little Thing Called Love by Queen", " Whitney Houston", "Sister Tharpe"];
+
+// array of all answers
+var answerChoices = ["Accademy Awards", "C.V Ramen", "Pulitzer", "Nobel Prize","Dance Pop", "Macarena", "The Cross Road", "UGK", "Baby Face", "Beatles", "Miles Davis", "LL Cool J", " Sgt. Pepper's Lonely Hearts Band", "With The Beatles", "Sound of Music", "Abbey Road", "Crazy Little Thing Called Love by Queen", "Starting Over by John Lennon", "Out of Touch by Hall & Oats", "Mickey by Toni Basil", " Whitney Houston", "Francesca Caccini", "Suzi Quatro", "Joan Jett", "Sister Tharpe", "Liona Boyd", "Kristin Hersh", "Bonnie Raitt" ]
